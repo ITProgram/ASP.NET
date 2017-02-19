73e79fb9983cfed28a2fa706d64443c1ee546dbc
ITProgram <Snpir.a-ius@ya.ru>
2017-02-20 01:44:04 +0300

    Lab 8 half

---
+++ 8/L08/L08/StudentControl.ascx
@@ -0,0 +1,36 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="StudentControl.ascx.cs" Inherits="L08.StudentControl" %>
+<div ">
+    <h1>StudentInfo</h1>
+    <p>
+        <asp:TextBox ID="TextBoxFamily" runat="server" placeholder="Фамилия"></asp:TextBox></p>
+    <p>
+        <asp:TextBox ID="TextBoxName" runat="server" placeholder="Имя"></asp:TextBox>
+    </p>
+    <p>
+        <asp:TextBox ID="TextBoxPatro" runat="server" placeholder="Отчество"></asp:TextBox>
+    </p>
+    <p>
+        <asp:TextBox ID="TextBoxDate" runat="server" placeholder="Дата рождения"></asp:TextBox>
+    </p>
+    <p>
+        <asp:RadioButtonList ID="RadioButtonListSex" runat="server" RepeatDirection="Horizontal" >
+            <asp:ListItem Value="0" Text="Ж" ></asp:ListItem>
+            <asp:ListItem Value="1" Text="М" Selected="true"></asp:ListItem>
+        </asp:RadioButtonList>
+    </p>
+    
+    <p>Факультет
+        <asp:DropDownList ID="DropDownListFaculty" runat="server" >
+            <asp:ListItem Value="ИТ"></asp:ListItem>
+            <asp:ListItem Value="ПИМ"></asp:ListItem>
+            <asp:ListItem Value="ХТиТ"></asp:ListItem>
+        </asp:DropDownList>
+    </p>
+    <p>
+        <asp:TextBox ID="TextBoxYear" runat="server" placeholder="Год поступления"></asp:TextBox>
+    </p>
+<p>
+        <asp:TextBox ID="TextBoxGroup" runat="server" placeholder="Группа"></asp:TextBox>
+    </p>
+    <asp:Button ID="ButtonCancel" runat="server" Text="Отказаться" /><asp:Button ID="ButtonEnter" runat="server" Text="Ввод" />
+</div>
---
+++ 8/L08/L08/StudentControl.ascx.designer.cs
@@ -0,0 +1,105 @@
+﻿//------------------------------------------------------------------------------
+// <автоматически создаваемое>
+//     Этот код создан программой.
+//
+//     Изменения в этом файле могут привести к неправильной работе и будут потеряны в случае
+//     повторной генерации кода. 
+// </автоматически создаваемое>
+//------------------------------------------------------------------------------
+
+namespace L08 {
+    
+    
+    public partial class StudentControl {
+        
+        /// <summary>
+        /// TextBoxFamily элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxFamily;
+        
+        /// <summary>
+        /// TextBoxName элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxName;
+        
+        /// <summary>
+        /// TextBoxPatro элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxPatro;
+        
+        /// <summary>
+        /// TextBoxDate элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxDate;
+        
+        /// <summary>
+        /// RadioButtonListSex элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList RadioButtonListSex;
+        
+        /// <summary>
+        /// DropDownListFaculty элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownListFaculty;
+        
+        /// <summary>
+        /// TextBoxYear элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxYear;
+        
+        /// <summary>
+        /// TextBoxGroup элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxGroup;
+        
+        /// <summary>
+        /// ButtonCancel элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ButtonCancel;
+        
+        /// <summary>
+        /// ButtonEnter элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ButtonEnter;
+    }
+}
