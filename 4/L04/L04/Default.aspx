1287b1ebb7bb2b510dbbeaea191aaee9e96a554b
ITProgram <Snpir.a-ius@ya.ru>
2017-02-20 12:38:52 +0300

    L04 Lab

--- 4/L04/L04/Default.aspx
+++ 4/L04/L04/Default.aspx
@@ -7,18 +7,18 @@
         <p>HTMLInputReset<input id="Reset1" type="reset" value="reset" runat="server" onserverclick="Reset_ButtonClick" /></p>
         <p>HTMLInputButton<input id="Button1" type="button" value="button" runat="server" onserverclick="ButtonClick" /></p>
         <p>HTMLInputSubmit<input id="Submit1" type="submit" value="submit" runat="server" onserverclick="Submit_ButtonClick" /></p>
-        <p>HTMLInputFile<input id="File1" type="file" runat="server" onserverclick="File_ButtonClick" /></p>
-        <p>HTMLInputText<input id="Text1" type="text" runat="server" onserverchange="Common_onserverchange" /></p>
-        <p>HTMLInputPassword<input id="Password1" type="password" runat="server" onserverchange="Common_onserverchange" /></p>
-        <p>HTMLInputCheckBox<input id="Checkbox1" type="checkbox" runat="server" OnServerClick="Common_onserverchange" /></p>
+        <p>HTMLInputFile<input id="File1" type="file" runat="server" onserverclick="File_ButtonClick" onserverchange="File_onserverchange"/></p>
+        <p>HTMLInputText<input id="Text1" type="text" runat="server" onserverchange="InputText_onserverchange" onserverclick="InputTextClick"/></p>
+        <p>HTMLInputPassword<input id="Password1" type="password" runat="server" onserverchange="Password_onserverchange" /></p>
+        <p>HTMLInputCheckBox<input id="Checkbox1" type="checkbox" runat="server" OnServerChange="CheckBox_onserverchange" /></p>
         <p>HTMLInputRadioButton
-            <input id="Radio1" type="radio" runat="server" onserverchange="Common_onserverchange" />
-            <input id="Radio2" type="radio" runat="server" onserverchange="Common_onserverchange" />
+            <input id="Radio1" type="radio" runat="server" onserverchange="radio1_onserverchange" />
+            <input id="Radio2" type="radio" runat="server" onserverchange="radio2_onserverchange" />
         </p>
-        <p>HTMLTextArea<textarea id="TextArea1" cols="20" rows="2" runat="server" onserverchange="Common_onserverchange"></textarea></p>
+        <p>HTMLTextArea<textarea id="TextArea1" cols="20" rows="2" runat="server" onserverchange="textArea_onserverchange"></textarea></p>
         
         <p>HTMLSelect
-            <select id="Select1" runat="server">
+            <select id="Select1" runat="server" onserverchange="Select1_ServerChange">
                 <option value="1">One</option>
                 <option value="2">Two</option>
                 <option value="3">Three</option>
--- 4/L04/L04/Default.aspx.cs
+++ 4/L04/L04/Default.aspx.cs
@@ -11,60 +11,136 @@ namespace L04
     public partial class _Default : Page
     {
         HtmlTable t = new HtmlTable();
-
+        HtmlTableRow r;
+        HtmlTableCell c;
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+
             t.Border = 1;
             t.CellPadding = 3;
             t.CellSpacing = 3;
             t.Align = "center";
             t.BorderColor = "green";
-            HtmlTableRow r;
-            HtmlTableCell c;
+
             r = new HtmlTableRow();
             c = new HtmlTableCell(); c.InnerHtml = "html Control"; r.Cells.Add(c);
             c = new HtmlTableCell(); c.InnerHtml = "onserverclick"; r.Cells.Add(c);
             c = new HtmlTableCell(); c.InnerHtml = "onserverchange"; r.Cells.Add(c);
             t.Rows.Add(r);
+            /*
             r = new HtmlTableRow();
             c = new HtmlTableCell(); c.InnerHtml = "html inputReset"; r.Cells.Add(c);
             c = new HtmlTableCell(); c.InnerHtml = "no"; r.Cells.Add(c);
             c = new HtmlTableCell(); c.InnerHtml = "no"; r.Cells.Add(c);
             t.Rows.Add(r);
+            */
             Controls.Add(t);
         }
         protected void Reset_ButtonClick(object sender, System.EventArgs e)
         {
-            Response.Write("HEllo Suchka");
+            r = new HtmlTableRow();
+            c = new HtmlTableCell(); c.InnerHtml = "HtmlInputReset"; r.Cells.Add(c);
+            c = new HtmlTableCell(); c.InnerHtml = "+"; r.Cells.Add(c);
+            c = new HtmlTableCell(); c.InnerHtml = "-"; r.Cells.Add(c);
+            t.Rows.Add(r);
         }
         protected void ButtonClick(object sender, System.EventArgs e)
         {
-            Response.Write("HEllo Suchka");
+            r = new HtmlTableRow();
+            c = new HtmlTableCell(); c.InnerHtml = "HtmlInputButton"; r.Cells.Add(c);
+            c = new HtmlTableCell(); c.InnerHtml = "+"; r.Cells.Add(c);
+            c = new HtmlTableCell(); c.InnerHtml = "-"; r.Cells.Add(c);
+            t.Rows.Add(r);
         }
         protected void Submit_ButtonClick(object sender, System.EventArgs e)
         {
-            Response.Write("HEllo Suchka");
-            
+            r = new HtmlTableRow();
+            c = new HtmlTableCell(); c.InnerHtml = "HtmlInputSubmit"; r.Cells.Add(c);
+            c = new HtmlTableCell(); c.InnerHtml = "+"; r.Cells.Add(c);
+            c = new HtmlTableCell(); c.InnerHtml = "-"; r.Cells.Add(c);
+            t.Rows.Add(r);
+
         }
         protected void File_ButtonClick(object sender, System.EventArgs e)
         {
-            Response.Write("HEllo Suchka");
+            r = new HtmlTableRow();
+            c = new HtmlTableCell(); c.InnerHtml = "HtmlInputFile"; r.Cells.Add(c);
+            c = new HtmlTableCell(); c.InnerHtml = "+"; r.Cells.Add(c);
+            c = new HtmlTableCell(); c.InnerHtml = "-"; r.Cells.Add(c);
+            t.Rows.Add(r);
+        }
+        protected void File_onserverchange(object sender, System.EventArgs e)
+        {
+            r = new HtmlTableRow();
+            c = new HtmlTableCell(); c.InnerHtml = "HtmlInputFile"; r.Cells.Add(c);
+            c = new HtmlTableCell(); c.InnerHtml = "-"; r.Cells.Add(c);
+            c = new HtmlTableCell(); c.InnerHtml = "+"; r.Cells.Add(c);
+            t.Rows.Add(r);
+
         }
         protected void InputTextClick(object sender, System.EventArgs e)
         {
-            Response.Write("HEllo Suchka");
+            r = new HtmlTableRow();
+            c = new HtmlTableCell(); c.InnerHtml = "HtmlInputText"; r.Cells.Add(c);
+            c = new HtmlTableCell(); c.InnerHtml = "+"; r.Cells.Add(c);
+            c = new HtmlTableCell(); c.InnerHtml = "-"; r.Cells.Add(c);
+            t.Rows.Add(r);
         }
-        protected void Common_onserverchange(object sender, System.EventArgs e)
+        protected void InputText_onserverchange(object sender, System.EventArgs e)
         {
-
-                Response.Write("comm");
+            r = new HtmlTableRow();
+            c = new HtmlTableCell(); c.InnerHtml = "HtmlInputText"; r.Cells.Add(c);
+            c = new HtmlTableCell(); c.InnerHtml = "-"; r.Cells.Add(c);
+            c = new HtmlTableCell(); c.InnerHtml = "+"; r.Cells.Add(c);
+            t.Rows.Add(r);
         }
-        protected void Com(object sender, System.EventArgs e)
+        protected void Password_onserverchange(object sender, System.EventArgs e)
         {
-
-            Response.Write("COMMOMMMMMM");
+            r = new HtmlTableRow();
+            c = new HtmlTableCell(); c.InnerHtml = "HtmlInputPassword"; r.Cells.Add(c);
+            c = new HtmlTableCell(); c.InnerHtml = "-"; r.Cells.Add(c);
+            c = new HtmlTableCell(); c.InnerHtml = "+"; r.Cells.Add(c);
+            t.Rows.Add(r);
+        }
+        protected void CheckBox_onserverchange(object sender, System.EventArgs e)
+        {
+            r = new HtmlTableRow();
+            c = new HtmlTableCell(); c.InnerHtml = "HtmlInputCheckBox"; r.Cells.Add(c);
+            c = new HtmlTableCell(); c.InnerHtml = "-"; r.Cells.Add(c);
+            c = new HtmlTableCell(); c.InnerHtml = "+"; r.Cells.Add(c);
+            t.Rows.Add(r);
+        }
+        protected void textArea_onserverchange(object sender, System.EventArgs e)
+        {
+            r = new HtmlTableRow();
+            c = new HtmlTableCell(); c.InnerHtml = "HtmlInputTextArea"; r.Cells.Add(c);
+            c = new HtmlTableCell(); c.InnerHtml = "-"; r.Cells.Add(c);
+            c = new HtmlTableCell(); c.InnerHtml = "+"; r.Cells.Add(c);
+            t.Rows.Add(r);
+        }
+        protected void Select1_ServerChange(object sender, EventArgs e)
+        {
+            r = new HtmlTableRow();
+            c = new HtmlTableCell(); c.InnerHtml = "HtmlInputSelect"; r.Cells.Add(c);
+            c = new HtmlTableCell(); c.InnerHtml = "-"; r.Cells.Add(c);
+            c = new HtmlTableCell(); c.InnerHtml = "+"; r.Cells.Add(c);
+            t.Rows.Add(r);
+        }
+        protected void radio1_onserverchange(object sender, EventArgs e)
+        {
+            r = new HtmlTableRow();
+            c = new HtmlTableCell(); c.InnerHtml = "HtmlInputRadioButton 1"; r.Cells.Add(c);
+            c = new HtmlTableCell(); c.InnerHtml = "-"; r.Cells.Add(c);
+            c = new HtmlTableCell(); c.InnerHtml = "+"; r.Cells.Add(c);
+            t.Rows.Add(r);
+        }
+        protected void radio2_onserverchange(object sender, EventArgs e)
+        {
+            r = new HtmlTableRow();
+            c = new HtmlTableCell(); c.InnerHtml = "HtmlInputRadioButton 2"; r.Cells.Add(c);
+            c = new HtmlTableCell(); c.InnerHtml = "-"; r.Cells.Add(c);
+            c = new HtmlTableCell(); c.InnerHtml = "+"; r.Cells.Add(c);
+            t.Rows.Add(r);
         }
     }
-
 }
