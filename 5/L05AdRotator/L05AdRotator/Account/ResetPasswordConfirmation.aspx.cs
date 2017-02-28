e84248c84b755cbdfd1ae4e98fc678e22d4f98ac
ITProgram <Snpir.a-ius@ya.ru>
2017-02-28 22:30:32 +0300

    L05 removes

--- 5/L05AdRotator/L05AdRotator/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace L05AdRotator.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- 5/L05AdRotator/L05AdRotator/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="L05AdRotator.Global" Language="C#" %>
