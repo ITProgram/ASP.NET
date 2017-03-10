1792fefa600c1d9df07ac793e820e7e5d01add4b
ITProgram <Snpir.a-ius@ya.ru>
2017-03-10 22:15:10 +0300

    L14

---
+++ 14/L14/L14/Controllers/PhoneController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace L14.Controllers
+{
+    public class PhoneController : Controller
+    {
+        // GET: Phone
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index(int a)
+        {
+            return View(a);
+        }
+        public ActionResult Index(string a)
+        {
+            return View(a+"string");
+        }
+    }
+}
